0c83417497b06d31946cc19dc57aa745765452b1
Priyanshu Agarwal <priyanshuone6@gmail.com>
2022-11-26 12:56:27 +0530

    Add comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
-
 class AirplaneSeating:
+    """
+    Write here
+    """
 
     def __init__(self, nested_arr, passengers):
         self.nested_arr = nested_arr
@@ -8,71 +9,53 @@ class AirplaneSeating:
 
     def build_layout(self):
         layout = []
+        # Create a matrix from the nested array
         for arr in self.nested_arr:
             cols = arr[0]
             rows = arr[1]
 
+            # Initialize the matrix with -1
             matrix = []
             for _ in range(rows):
                 matrix.append([-1] * cols)
             layout.append(matrix)
 
-        #print(layout[0][0][0])
-        #print("\n")
-
         return layout
 
-
-    def assign_middle_seats(self, layout):
-
+    def assign_seats(self, layout):
 
         for i in range(len(layout)):
-            #print(i)
+            # Parse through the nested list
             for j in range(len(layout[i])):
-                #print(i,"||||||",j)
-                #print(layout[i][j][-1])
 
-                for k in range(1, len(layout[i][j])-1):
-
+                # Set the elements between the index 0 and -1 to a middle seat
+                for k in range(1, len(layout[i][j]) - 1):
                     layout[i][j][k] = "Middle"
 
+                # Set the first and last elements to a aisle seat
                 layout[i][j][0] = "Aisle"
                 layout[i][j][-1] = "Aisle"
 
-                #print(i,len(layout))
-
-                if i==0:
-                    layout[0][j][0] = "Windows"
-                if i==len(layout)-1:
-                    #print("\n",j,"|||||||||")
-                    layout[-1][j][-1] = "Windows"
+                # Override the first and last element of the nested list to a window seat
+                if i == 0:
+                    layout[0][j][0] = "Window"
+                if i == len(layout) - 1:
+                    layout[-1][j][-1] = "Window"
 
         return layout
 
+    # def assign_seat_number(self, layout):
+    #    for seat in layout[0]:
+    #        print(seat)
 
 
 if __name__ == "__main__":
-    a = [[3,2], [4,3], [2,3], [3,4]]
-    airplane_class = AirplaneSeating(a, 30)
-    layout_fun = airplane_class.build_layout()
-    middle_seats = airplane_class.assign_middle_seats(layout_fun)
+    input_array = [[3, 2], [4, 3], [2, 3], [3, 4]]
+    input_passengers = 30
 
-    print(middle_seats)
+    airplane_class = AirplaneSeating(input_array, input_passengers)
+    layout_func = airplane_class.build_layout()
+    seats_func = airplane_class.assign_seats(layout_func)
+    seat_num_func = airplane_class.assign_seat_number(seats_func)
 
-"""
-import numpy
-
-a = [[3,2], [4,3], [2,3], [3,4]]
-a = numpy.ndarray(a)
-
-
-for i in a:
-    print(numpy.zeros(i),"\n")
-
-
-
-for i in a:
-    for j in i:
-        print(j, end = " ")
-    print()
-"""
+    print(seat_num_func)
