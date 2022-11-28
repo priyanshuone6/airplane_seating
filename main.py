8a9cb2614e66f4c745c33f4720d46acfd911196e
Priyanshu Agarwal <priyanshuone6@gmail.com>
2022-11-28 18:41:17 +0530

    Make layout an instance variable

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class AirplaneSeating:
     def __init__(self, nested_arr, passengers):
         self.nested_arr = nested_arr
         self.passengers = passengers
+        self.layout = []
 
         # Initialize the seat number
         self.seat_number = 1
@@ -35,7 +36,6 @@ class AirplaneSeating:
         Builds the layout of the flight from the input array by initializing
         matrix with -1.
         """
-        layout = []
         # Create a matrix from the nested array
         for arr in self.nested_arr:
             cols, rows = arr
@@ -44,71 +44,65 @@ class AirplaneSeating:
             matrix = []
             for _ in range(rows):
                 matrix.append([-1] * cols)
-            layout.append(matrix)
+            self.layout.append(matrix)
 
-        return layout
+        return self
 
-    def assign_aisle_seats(self, layout):
+    def assign_aisle_seats(self):
         """Assigns the aisle seats to the passengers."""
-        for i in range(len(layout)):
+        for i in range(len(self.layout)):
             # Parse through the nested list
-            for j in range(len(layout[i])):
+            for j in range(len(self.layout[i])):
 
                 # Skip window seats
                 if not (i == 0 and i == 1):
                     # Set first and last elements of sub list to a aisle seat
-                    layout[i][j][0] = f"{self.seat_number} Aisle"
+                    self.layout[i][j][0] = f"{self.seat_number} Aisle"
                     self.seat_number += 1
-                    layout[i][j][-1] = f"{self.seat_number} Aisle"
+                    self.layout[i][j][-1] = f"{self.seat_number} Aisle"
                     self.seat_number += 1
 
                     if self.seat_number == self.passengers:
                         break
 
-        return layout
-
-    def assign_window_seats(self, layout):
+    def assign_window_seats(self):
         """Assigns the window seats to the passengers."""
-        for i in range(len(layout)):
+        for i in range(len(self.layout)):
             # Parse through the nested list
-            for j in range(len(layout[i])):
+            for j in range(len(self.layout[i])):
 
                 # Set first and last element of the nested list to a window seat
                 if i == 0:
-                    layout[0][j][0] = f"{self.seat_number} Window"
+                    self.layout[0][j][0] = f"{self.seat_number} Window"
                     self.seat_number += 1
-                if i == len(layout) - 1:
-                    layout[-1][j][-1] = f"{self.seat_number} Window"
+                if i == len(self.layout) - 1:
+                    self.layout[-1][j][-1] = f"{self.seat_number} Window"
                     self.seat_number += 1
 
                 if self.seat_number == self.passengers:
                     break
 
-        return layout
-
-    def assign_middle_seats(self, layout):
+    def assign_middle_seats(self):
         """Assigns the middle seats to the passengers."""
-        for i in range(len(layout)):
+        for i in range(len(self.layout)):
             # Parse through the nested list
-            for j in range(len(layout[i])):
+            for j in range(len(self.layout[i])):
                 # Set the elements between the index 0 and -1 to a middle seat
-                for k in range(1, len(layout[i][j]) - 1):
-                    layout[i][j][k] = f"{self.seat_number} Middle"
+                for k in range(1, len(self.layout[i][j]) - 1):
+                    self.layout[i][j][k] = f"{self.seat_number} Middle"
                     self.seat_number += 1
 
                     if self.seat_number == self.passengers:
                         break
-
-        return layout
 
 
 def main(input_array, input_passengers):
     """Function to run the program."""
     airplane_class = AirplaneSeating(input_array, input_passengers)
     layout_func = airplane_class.build_layout()
-    aisle_func = airplane_class.assign_aisle_seats(layout_func)
-    window_func = airplane_class.assign_window_seats(aisle_func)
-    middle_func = airplane_class.assign_middle_seats(window_func)
+    aisle_func = airplane_class.assign_aisle_seats()
+    window_func = airplane_class.assign_window_seats()
+    middle_func = airplane_class.assign_middle_seats()
 
     return middle_func
 
