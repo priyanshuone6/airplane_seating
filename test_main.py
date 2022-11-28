88c6297585ab896a0b55f4cfe64a92d2b5836b00
Priyanshu Agarwal <priyanshuone6@gmail.com>
2022-11-28 20:28:52 +0530

    Add a test for the fill function

--- test_main.py
+++ test_main.py
@@ -6,6 +6,25 @@ import unittest
 import main
 
 
+INITIAL_LAYOUT = """\
+Airplane Layout
+===============
+ 0  0  0     0  0  0  0     0  0     0  0  0
+ 0  0  0     0  0  0  0     0  0     0  0  0
+             0  0  0  0     0  0     0  0  0
+                                     0  0  0\
+"""
+
+FINAL_LAYOUT = """\
+Airplane Layout
+===============
+19 25  1     2 26 27  3     4  5     6 28 20
+21 29  7     8 30  0  9    10 11    12  0 22
+            13  0  0 14    15 16    17  0 23
+                                    18  0 24\
+"""
+
+
 class TestAirplane(unittest.TestCase):
     def test_input(self):
 
@@ -16,7 +35,6 @@ class TestAirplane(unittest.TestCase):
         # Check if dims are in the correct format
         with self.assertRaises(ValueError):
             main.Airplane([1, 2, 3], 3)
-
 
         # Check if the passenger input is a number
         with self.assertRaises(TypeError):
@@ -33,6 +51,14 @@ class TestAirplane(unittest.TestCase):
         # Check if we have enough capacity
         with self.assertRaises(ValueError):
             main.Airplane([[3, 2], [4, 3], [2, 3], [3, 4]], 37)
+
+    def test_successful_layout(self):
+        airplane = main.Airplane([[3, 2], [4, 3], [2, 3], [3, 4]], 30)
+        initial_layout = str(airplane)
+        self.assertEqual(initial_layout, INITIAL_LAYOUT)
+        airplane.fill()
+        final_layout = str(airplane)
+        self.assertEqual(final_layout, FINAL_LAYOUT)
 
 
 if __name__ == "__main__":
