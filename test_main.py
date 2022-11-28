0fa79dc3d2dabc6a5ed984aed9ebdf73a094bf69
Priyanshu Agarwal <priyanshuone6@gmail.com>
2022-11-28 20:23:24 +0530

    Update tests for input validation

--- test_main.py
+++ test_main.py
@@ -6,28 +6,33 @@ import unittest
 import main
 
 
-class TestAirplaneSeating(unittest.TestCase):
+class TestAirplane(unittest.TestCase):
     def test_input(self):
 
         # Check if the passenger input is not an integer
         with self.assertRaises(TypeError):
-            main.AirplaneSeating([1, 2, 3], "volopay")
+            main.Airplane(None, "volopay")
+
+        # Check if dims are in the correct format
+        with self.assertRaises(ValueError):
+            main.Airplane([1, 2, 3], 3)
+
+
+        # Check if the passenger input is a number
+        with self.assertRaises(TypeError):
+            main.Airplane([[1, 2]], None)
 
         # Check if the passenger input is negative integer
         with self.assertRaises(ValueError):
-            main.AirplaneSeating([1, 2, 3], -1)
-
-        # Check if the passenger input is empty
-        with self.assertRaises(ValueError):
-            main.AirplaneSeating([1, 2, 3], None)
+            main.Airplane([[1, 2]], -3)
 
         # Check if the array input is empty
         with self.assertRaises(ValueError):
-            main.AirplaneSeating([], 20)
+            main.Airplane([], 20)
 
-        # Check if the array input is a sequence
-        with self.assertRaises(TypeError):
-            main.AirplaneSeating(10, 20)
+        # Check if we have enough capacity
+        with self.assertRaises(ValueError):
+            main.Airplane([[3, 2], [4, 3], [2, 3], [3, 4]], 37)
 
 
 if __name__ == "__main__":
