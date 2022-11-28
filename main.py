b0f6f2a1f8752bb0f8aefd21618e26fa37bc9f4b
Priyanshu Agarwal <priyanshuone6@gmail.com>
2022-11-28 23:34:26 +0530

    Add comments

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ class Airplane:
         if not isinstance(self.dims, list):
             raise TypeError("Given dimensions must be a list")
 
+        # Raise an error if array is empty
         if len(self.dims) == 0:
             raise ValueError("Airplane must have some seats")
 
-        # Raise an error if dims is not proper
+        # Raise an error if dims is not a list or length of dims is less than 2
         for item in self.dims:
             if not isinstance(item, list) or len(item) != 2:
                 raise ValueError(
-                    f"Expected dims to contain arrays of length 2, got '{item}'")
+                    f"Expected dimmensions to contain arrays of length 2, got '{item}'"
+                )
 
         # Raise an error if passengers is not an integer
         if not isinstance(self.num_passengers, int):
@@ -34,31 +36,38 @@ class Airplane:
         if self.num_passengers < 0:
             raise ValueError("Passengers cannot be negative")
 
+        # Raise an error if passengers are more than the total number of seats
         total_capacity = sum([dim[0] * dim[1] for dim in self.dims])
         if self.num_passengers > total_capacity:
             raise ValueError(
-                f"Plane doesn't have enough seats for {self.num_passengers} passengers")
+                f"Plane doesn't have enough seats for {self.num_passengers} passengers"
+            )
 
     def __str__(self):
         """Pretty print the airport layout with passengers."""
-        # get the length of the biggest passenger number to properly indent everything
+        # Get the length of the biggest passenger number to properly indent everything
         indent = len(str(self.num_passengers))
-        layout_heading = 'Airplane Layout'
-        seating_rows = [layout_heading, '=' * len(layout_heading)]
+
+        # Print Airplane layout
+        #       ===============
+        layout_heading = "Airplane Layout"
+        seating_rows = [layout_heading, "=" * len(layout_heading)]
+
+        # This is to make sure the output is aligned
         for row_idx in range(self.max_rows):
 
             row_str = []
             for arr in self.layout:
                 if row_idx < len(arr):
-                    arr_row = [f'{num:>{indent}}' for num in arr[row_idx]]
+                    arr_row = [f"{num:>{indent}}" for num in arr[row_idx]]
                 else:
-                    arr_row = [' ' * indent for _ in range(len(arr[0]))]
-                row_str.append(' '.join(arr_row))
+                    arr_row = [" " * indent for _ in range(len(arr[0]))]
+                row_str.append(" ".join(arr_row))
 
             # Add 2 extra spaces between seat groups than the length of the biggest passenger num
-            seating_rows.append((' ' * (indent + 2)).join(row_str))
+            seating_rows.append((" " * (indent + 2)).join(row_str))
 
-        return '\n'.join(seating_rows)
+        return "\n".join(seating_rows)
 
     def _build_layout(self):
         """
@@ -68,7 +77,7 @@ class Airplane:
         # Create a matrix from the nested array
         for arr in self.dims:
             cols, rows = arr
-            # Initialize the matrix with 0
+            # Initialize the matrix with 0 which represents empty seats
             matrix = [[0] * cols for _ in range(rows)]
             self._layout.append(matrix)
 
@@ -82,7 +91,7 @@ class Airplane:
 
     @property
     def max_rows(self):
-        """Maximum number of rows in any seating group."""
+        """Returns maximum number of rows in any seating group."""
         return max(map(len, self.layout))
 
     def all_passengers_seated(self):
@@ -109,6 +118,7 @@ class Airplane:
             if seat_group_num != 0:
                 self.passengers_seated += 1
                 seat_group[row_num][0] = self.passengers_seated
+            # Break if passengers are greater or equal than total seats
             if self.all_passengers_seated():
                 break
 
@@ -116,22 +126,26 @@ class Airplane:
             if seat_group_num != len(self.layout) - 1:
                 self.passengers_seated += 1
                 seat_group[row_num][-1] = self.passengers_seated
+            # Break if passengers are greater or equal than total seats
             if self.all_passengers_seated():
                 break
 
     def assign_window_seats(self):
         """Assigns the window seats to the passengers."""
+        # Loop through the rows
         for row_num, seat_group_num in self.traverse():
+            # Break if passengers are greater or equal than total seats
             if self.all_passengers_seated():
                 break
 
             seat_group = self.layout[seat_group_num]
-
+            # Set window seats for the first seat group
             if seat_group_num == 0:
                 self.passengers_seated += 1
                 seat_group[row_num][0] = self.passengers_seated
                 continue
 
+            # Set window seats for the last seat group
             if seat_group_num == len(self.layout) - 1:
                 self.passengers_seated += 1
                 seat_group[row_num][-1] = self.passengers_seated
@@ -139,9 +153,12 @@ class Airplane:
 
     def assign_middle_seats(self):
         """Assigns the middle seats to the passengers."""
+        # Loop through the rows
         for row_num, seat_group_num in self.traverse():
             seat_group = self.layout[seat_group_num]
             cols, _ = self.dims[seat_group_num]
+
+            # Set the middle seats for seats between second and second last column
             for col_num in range(1, cols - 1):
                 if not self.all_passengers_seated():
                     self.passengers_seated += 1
